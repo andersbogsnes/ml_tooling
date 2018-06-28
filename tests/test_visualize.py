d68edd6c4221c11f4e7c191ed4d80295fe09ca1e
Anders Bogsnes <andersbogsnes@gmail.com>
2018-06-28 15:15:45 +0200

    Added tests for viz

--- tests/test_visualize.py
+++ tests/test_visualize.py
@@ -1,4 +1,5 @@
 import pytest
+import numpy as np
 from matplotlib.axes import Axes
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
@@ -8,6 +9,8 @@ from ml_utils.visualizations.visualizations import (RegressionVisualize,
                                                     ClassificationVisualize,
                                                     VizError)
 from sklearn.svm import SVC
+
+np.random.seed(42)
 
 
 def test_result_regression_gets_correct_visualizers(regression):
@@ -26,6 +29,37 @@ def test_regression_visualize_has_all_plots(attr, regression):
     plotter = getattr(result, attr)()
     assert isinstance(plotter, Axes)
 
+
+def test_confusion_matrix_plots_have_correct_data(classifier):
+    ax = classifier.result.plot.confusion_matrix()
+
+    assert 'Confusion Matrix - LogisticRegression - Normalized' == ax.title._text
+    result = {text._text for text in ax.texts}
+    assert {'0.16', '0.38', '0.62', '0.84'} == result
+    assert 'True Label' == ax.get_ylabel()
+    assert 'Predicted Label' == ax.get_xlabel()
+
+
+def test_feature_importance_plots_have_correct_data(classifier):
+    ax = classifier.result.plot.feature_importance()
+
+    expected = {'-1.76', '-0.83', '0.34', '0.60'}
+    assert 'Feature Importance - LogisticRegression' == ax.title._text
+    assert expected == {text._text for text in ax.texts}
+    assert 'Features' == ax.get_ylabel()
+    assert 'Importance' == ax.get_xlabel()
+
+
+def test_lift_curve_have_correct_data(classifier):
+    ax = classifier.result.plot.lift_curve()
+
+    assert 'Lift Curve - LogisticRegression' == ax.title._text
+    assert 'Lift' == ax.get_ylabel()
+    assert '% of Data' == ax.get_xlabel()
+    assert pytest.approx(19.5) == np.sum(ax.lines[0].get_xdata())
+    assert pytest.approx(48.67, rel=.0001) == np.sum(ax.lines[0].get_ydata())
+
+def test
 
 @pytest.mark.parametrize('attr', ['confusion_matrix',
                                   'roc_curve',
@@ -58,3 +92,14 @@ def test_lift_chart_fails_correctly_with_2d_proba():
     proba = clf.predict_proba(x)
     with pytest.raises(VizError):
         plot_lift_chart(y, proba)
+
+
+def test_viz_get_feature_importance_returns_coef_from_regression(regression):
+    viz = RegressionVisualize(regression.model,
+                              regression.config,
+                              regression.x,
+                              regression.y,
+                              regression.x,
+                              regression.y)
+    importance = viz._get_feature_importance()
+    assert np.all(regression.model.coef_ == importance)

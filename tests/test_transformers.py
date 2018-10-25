b522a716d97d062193caf116c7bbf920340d1137
Anders Bogsnes <andersbogsnes@gmail.com>
2018-10-25 10:56:03 +0200

    Update tests/test_transformers.py
    Co-Authored-By: thomasfrederikhoeck <tfhoeck@gmail.com>

--- tests/test_transformers.py
+++ tests/test_transformers.py
@@ -42,7 +42,7 @@ def test_df_selector_with_multiple_columns(categorical):
 def test_df_selector_raise_missing_column(categorical):
     select = Select(['category_a', 'category_b', 'category_c'])
 
-    with pytest.raises(TransformerError):
+    with pytest.raises(TransformerError, match="The DataFrame does not include the columns: category_c):
         select.fit_transform(categorical)
 
 

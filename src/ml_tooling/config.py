e3bc54909227430ea45135490a2b8640fb64fa81
Anders Bogsnes <andersbogsnes@gmail.com>
2018-11-04 18:57:11 +0100

    Fixed Train-size to Test-size

--- src/ml_tooling/config.py
+++ src/ml_tooling/config.py
@@ -15,7 +15,7 @@ class DefaultConfig:
         self.CROSS_VALIDATION = 10
         self.STYLE_SHEET = MPL_STYLESHEET
         self.N_JOBS = -1
-        self.TRAIN_SIZE = 0.25
+        self.TEST_SIZE = 0.25
         self.RANDOM_STATE = 42
 
     def __repr__(self):

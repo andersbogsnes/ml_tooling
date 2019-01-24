aa00e9b7a3cd4cf4b60fc2f27cd8ad24b5af9a41
Thomas Frederik Hoeck <44194839+thomasfrederikhoeck@users.noreply.github.com>
2019-01-24 14:47:11 +0100

    Fix plot lift curve index (#172)
    * Added extraction of values from y_true
    
    * Added to CHANGELOG
    
    * Removed unnecessary pass statement
    
    * Update src/ml_tooling/plots.py
    
    cast to numpy
    
    Co-Authored-By: thomasfrederikhoeck <44194839+thomasfrederikhoeck@users.noreply.github.com>

--- src/ml_tooling/plots.py
+++ src/ml_tooling/plots.py
@@ -303,6 +303,9 @@ def plot_lift_curve(y_true, y_proba, title=None, ax=None):
 
     title = "Lift Curve" if title is None else title
 
+    # Get numpy array as _cum_gain_curve takes numpy arrays
+    y_true = np.array(y_true)
+
     percents, gains = _cum_gain_curve(y_true, y_proba)
     positives = np.where(y_proba > .5, 1, 0)
     score = metrics.lift_score(y_true, positives)
@@ -318,7 +321,6 @@ def plot_lift_curve(y_true, y_proba, title=None, ax=None):
 
 class VizError(Exception):
     """Base Exception for visualization errors"""
-    pass
 
 
 def _cum_gain_curve(y_true: np.ndarray,

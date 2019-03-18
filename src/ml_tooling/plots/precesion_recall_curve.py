63a564a7d3ea44debbbaac6f1bc175c37f047143
Thomas Frederik Hoeck <abthhk@almbrand.dk>
2019-03-18 16:37:52 +0100

    Added Precision-Recall curve to close #8.

---
+++ src/ml_tooling/plots/precesion_recall_curve.py
@@ -0,0 +1,44 @@
+from matplotlib import pyplot as plt
+from matplotlib.axes import Axes
+from sklearn.metrics import precision_recall_curve, average_precision_score
+
+from ml_tooling.utils import DataType
+
+
+def plot_pr_curve(y_true: DataType, y_proba: DataType, title: str = None, ax: Axes = None) -> Axes:
+    """
+    Plot precsion-recall curve. Works only with probabilities
+
+    :param y_true:
+        True labels
+
+    :param y_proba:
+        Probability estimate from model
+
+    :param title:
+        Plot title
+
+    :param ax:
+        Pass in your own ax
+
+    :return:
+        matplotlib.Axes
+    """
+    title = 'Precision-Recall curve' if title is None else title
+
+    precision, recall, _ = precision_recall_curve(y_true, y_proba)
+    score = average_precision_score(y_true, y_proba)
+
+    if ax is None:
+        fig, ax = plt.subplots()
+
+    ax.step(recall, precision, label=f"Average precision: {score}", alpha=0.95, where='post')
+    ax.fill_between(recall, precision, alpha=0.2, step='post')
+    ax.set_title(title)
+    ax.set_ylabel('Precision')
+    ax.set_xlabel('Recall')
+    ax.set_ylim([0.0, 1.05])
+    ax.set_xlim([0.0, 1.0])
+    ax.legend(loc='best')
+    plt.tight_layout()
+    return ax
--- src/ml_tooling/result/viz/classification_viz.py
+++ src/ml_tooling/result/viz/classification_viz.py
@@ -1,6 +1,6 @@
 from matplotlib import pyplot as plt
 
-from ml_tooling.plots import plot_confusion_matrix, plot_roc_auc, plot_lift_curve
+from ml_tooling.plots import plot_confusion_matrix, plot_roc_auc, plot_lift_curve, plot_pr_curve
 from ml_tooling.plots.utils import VizError
 from ml_tooling.result.viz import BaseVisualize
 
@@ -53,3 +53,19 @@ class ClassificationVisualize(BaseVisualize):
             title = f'Lift Curve - {self._model_name}'
             y_proba = self._model.predict_proba(self._data.test_x)[:, 1]
             return plot_lift_curve(self._data.test_y, y_proba, title=title, **kwargs)
+
+    def pr_curve(self, **kwargs) -> plt.Axes:
+        """
+        Visualize a Precision-Recall curve for a classification model.
+        Model must implement a `predict_proba` method
+        Any kwargs are passed onto matplotlib
+        :return:
+            matplotlib.Axes
+        """
+        if not hasattr(self._model, 'predict_proba'):
+            raise VizError("Model must provide a 'predict_proba' method")
+
+        with plt.style.context(self._config.STYLE_SHEET):
+            title = f'Precision-Recall - {self._model_name}'
+            y_proba = self._model.predict_proba(self._data.test_x)[:, 1]
+            return plot_pr_curve(self._data.test_y, y_proba, title=title, **kwargs)

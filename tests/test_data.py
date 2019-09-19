ef92dee506a9dbde9fa6fb01d5fbb8cdc27629ae
Anders Bogsnes <abanbn@almbrand.dk>
2019-09-19 17:41:40 +0200

    Added tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -6,7 +6,8 @@ from sklearn.datasets import load_boston
 import pandas as pd
 import sqlalchemy as sa
 from ml_tooling.data.sql import SQLDataSet
-from ml_tooling.utils import DataType
+from ml_tooling.data.file import FileDataSet
+from ml_tooling.utils import DataType, DataSetError
 
 
 @pytest.fixture
@@ -29,7 +30,7 @@ def test_db(test_df, test_engine):
 
 
 @pytest.fixture
-def test_data(test_db):
+def test_sqldata(test_db):
     class BostonDataSet(SQLDataSet):
         def load_training_data(self, *args, **kwargs) -> Tuple[DataType, DataType]:
             sql = "SELECT * FROM boston"
@@ -44,8 +45,24 @@ def test_data(test_db):
     return BostonDataSet(test_db)
 
 
-def test_tabledataset_works_correctly(test_data):
-    assert test_data._x is None
-    features = test_data.x
-    assert test_data._x is not None
+def test_sqldataset_works_correctly(test_sqldata):
+    assert test_sqldata._x is None
+    features = test_sqldata.x
+    assert test_sqldata._x is not None
     assert len(features) == 506
+
+
+def test_cant_modify_x_and_y(test_data):
+    with pytest.raises(DataSetError, match="Trying to modify x - x is immutable"):
+        test_data.x = "testx"
+    with pytest.raises(DataSetError, match="Trying to modify y - y is immutable"):
+        test_data.y = "testy"
+
+
+def test_can_save_to_another_dataset(test_data, tmp_path):
+    test_file = tmp_path / "test.parquet"
+    filedata = FileDataSet(test_file)
+    filedata.save()
+
+    result = pd.read_parquet(test_file)
+    assert result.drop(columns="MEDV") == test_data.x

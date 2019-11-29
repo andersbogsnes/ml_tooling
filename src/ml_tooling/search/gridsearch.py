0e7544b6eec38650a183fb7de9ff39bbcf91e614
Anders Bogsnes <abanbn@almbrand.dk>
2019-11-29 15:48:20 +0100

    Bug fixed
    Gridsearching only cloned once, so only the last configuration had an effect

--- src/ml_tooling/search/gridsearch.py
+++ src/ml_tooling/search/gridsearch.py
@@ -9,6 +9,5 @@ from ml_tooling.utils import Estimator
 def prepare_gridsearch_estimators(
     estimator: Estimator, params: dict
 ) -> Iterator[Estimator]:
-    baseline_estimator = clone(estimator)
     grid = ParameterGrid(params)
-    yield from (baseline_estimator.set_params(**params) for params in grid)
+    return [clone(estimator).set_params(**p) for p in grid]

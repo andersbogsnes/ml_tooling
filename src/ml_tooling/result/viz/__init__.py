d028b0fb4c1bdfadc6270cf716ea231bd01aea30
Anders Bogsnes <andersbogsnes@gmail.com>
2020-03-05 11:13:36 +0100

    Moved plotting class init to being a property (#453)

--- src/ml_tooling/result/viz/__init__.py
+++ src/ml_tooling/result/viz/__init__.py
@@ -3,15 +3,4 @@ from .regression_viz import RegressionVisualize
 from .classification_viz import ClassificationVisualize
 
 
-def create_plotter(model, data):
-    if model.is_classifier:
-        return ClassificationVisualize(model.estimator, data, model.config)
-    return RegressionVisualize(model.estimator, data, model.config)
-
-
-__all__ = [
-    "BaseVisualize",
-    "RegressionVisualize",
-    "ClassificationVisualize",
-    "create_plotter",
-]
+__all__ = ["BaseVisualize", "RegressionVisualize", "ClassificationVisualize"]

06f13d46a8a0af3d15c85366276f21fec1650116
ibenfjord <iben95@hotmail.dk>
2020-06-22 15:27:13 +0200

    Changed class definition

--- src/ml_tooling/data/demo_dataset.py
+++ src/ml_tooling/data/demo_dataset.py
@@ -16,8 +16,10 @@ from ml_tooling.utils import DatasetError
 
 def load_demo_dataset(dataset_name: str, **kwargs):
     """
-    Creates a data class inheriting from ML Tooling dataclas Dataset.
-    The demo data is imported from sklearn.datasets
+    The function calls either the load and fetch function from
+    :class:`sklearn.datasets`. Then a data class is created which is inheriting from
+    ML Tooling dataclas Dataset.
+    The options in kwargs can be read in :class:`sklearn.datasets`
 
     Parameters
     ----------
@@ -27,35 +29,7 @@ def load_demo_dataset(dataset_name: str, **kwargs):
         either parameter name or data_id needs to be specified. Possible dataset_name: 'iris',
         'boston', 'diabetes', 'digits', 'linnerud', 'wine', 'breast_cancer', 'openml'
 
-
     **kwargs:
-        name: str or None
-            String identifier of the dataset. Note that OpenML can have multiple datasets
-            with the same name.
-
-        version: integer or ‘active’, default=’active’
-            Version of the dataset. Can only be provided if also name is given. If
-            ‘active’ the oldest version that’s still active is used. Since there may be
-            more than one active version of a dataset, and those versions may fundamentally
-            be different from one another, setting an exact version is highly recommended.
-
-        data_id: int or None
-            OpenML ID of the dataset. The most specific way of retrieving a dataset. If
-            data_id is not given, name (and potential version) are used to obtain a dataset.
-
-        data_home: string or None, default None
-            Specify another download and cache folder for the data sets. By default all
-            scikit-learn data is stored in ‘~/scikit_learn_data’ subfolders.
-
-        target_column: string, list or None, default ‘default-target’
-            Specify the column name in the data to use as target. If ‘default-target’, the
-            standard target column a stored on the server is used. If None, all columns are
-            returned as data and the target is None. If list (of strings), all columns with
-            these names are returned as multi-target (Note: not all scikit-learn classifiers
-            can handle all types of multi-output combinations)
-
-        cache: boolean, default=True
-            Whether to cache downloaded datasets using joblib.
 
     Returns
     -------

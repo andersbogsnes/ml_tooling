78c6580a5d92804afb41e3f53ddd6b690c6c7fd3
ibenfjord <37499768+ibenfjord@users.noreply.github.com>
2020-06-29 12:24:25 +0200

    Update select.py

--- src/ml_tooling/transformers/select.py
+++ src/ml_tooling/transformers/select.py
@@ -11,7 +11,7 @@ class Select(BaseEstimator, TransformerMixin):
     Selects columns from DataFrame
     """
 
-    def __init__(self, columns: Union[List[str], str] = None):
+    def __init__(self, columns: Union[List[str], str]):
         """
         Parameters
         ----------

65ff7a36b70f8ba9e250916ce772bc21aae16bfa
ibenfjord <37499768+ibenfjord@users.noreply.github.com>
2020-06-29 10:47:21 +0200

    Update binner.py

--- src/ml_tooling/transformers/binner.py
+++ src/ml_tooling/transformers/binner.py
@@ -21,8 +21,6 @@ class Binner(BaseEstimator, TransformerMixin):
             x is extended by .1% on each side to include the minimum and maximum values of x.
             If a list is passed, defines the bin edges allowing for non-uniform width and no 
             extension of the range of x is done.
-
-
         labels: list
             Specifies the labels for the returned bins. Must be the same length as the
             resulting bins.

7d041144e95aaf9e6ef91414fb403a9ec4222e2e
ibenfjord <iben95@hotmail.dk>
2020-07-27 16:03:42 +0200

    Advised changes have been made Some of the changes which was recommened have benn made in target_feature_distribution.py file

--- src/ml_tooling/plots/target_feature_distribution.py
+++ src/ml_tooling/plots/target_feature_distribution.py
@@ -1,72 +1,91 @@
 from matplotlib import pyplot as plt
 from matplotlib.axes import Axes
-import pandas as pd
 import numpy as np
 
 from ml_tooling.utils import DataType
-from scipy import stats
+from ml_tooling.plots.utils import _plot_barh
 
 
-def target_feature_distribution(
-    data: pd.DataFrame,
-    targets: DataType,
-    features: DataType,
-    title: str = None,
-    labels: list = None,
-    median: bool = False,
+class MLToolingError(Exception):
+    """Error which occurs when using ML Tooling"""
+
+
+class VizError(MLToolingError):
+    """Error which occurs when using a Visualization"""
+
+
+def plot_target_feature_distribution(
+    target: DataType,
+    feature: DataType,
+    title: str = "Target feature distribution",
+    method: str = "mean",
+    ax: plt.Axes = None,
+    n_boots: int = 1000,
 ) -> Axes:
+    """
+    Creates a plot which compares the mean or median
+    of a binary target based on the given category features.
+
+    Parameters
+    ----------
+    target: np.Array or pd.Series
+        Target to compare with feature
+    feature: np.Array or pd.Series
+        Feature to compare with target
+    title: str
+        Title of graph
+    method: str
+        Which method to compare with. Support one of 'median' and 'mean'.
+    ax: plt.Axes
+        Matplotlib axes to draw the graph on. Creates a new one by default
+    n_boots: int
+        The number of boostrap iterations to use.
+    Returns
+    -------
+    plt.Axes
 
     """
-    Creates two plots, one which compares the mean or median
-    of a target based on the given features and another which shows
-    the distribution of the target based on the given features
+    if 0 and 1 not in np.unique(target):
+        raise VizError("Target feature distribution plot only works for binary target")
 
-    :param data:
-        DataFrame containing the data
+    method_mapping = {"mean": np.mean, "median": np.median}
 
-    :param targets:
-        The name of the column containing the targets
+    selected_method = method_mapping[method]
 
-    :param features:
-        The name of the column containing the features
+    feature_categories = np.unique(feature)
 
-    :param title:
-        Title for plot
-
-    :param labels:
-        Pass custom list of labels
-
-    :param median:
-        Whether to compare using median or mean, true for median
-
-    :return:
-        matplotlib.Axes
-    """
-
-    title = f"Survival Distributions: {targets}" if title is None else title
-    fig, axs = plt.subplots(nrows=1, ncols=2)
-    fig.suptitle(title)
-
-    ax1df = data.groupby(by=targets)[features]
-    std = ax1df.std()
-    count = ax1df.count()
-    yerr = std / np.sqrt(count) * stats.t.ppf(1 - 0.05 / 2, count - 1)
-    if median:
-        ax1df.median().plot(kind="bar", rot=0, ax=axs[0], yerr=yerr)
-        axs[0].axhline(y=np.median(data[features]), linestyle="--", color="red")
-        axs[0].set_title(f"Percentage {data[features].name} compared to median")
-    else:
-        ax1df.mean().plot(kind="bar", rot=0, ax=axs[0], yerr=yerr)
-        axs[0].axhline(y=np.mean(data[features]), linestyle="--", color="red")
-        axs[0].set_title(f"Percentage {data[features].name} compared to mean")
-
-    # Is this plot even needed? It doesn't add anything related to the issue
-    data.groupby(features)[targets].value_counts().unstack(0).plot(
-        kind="bar", rot=0, ax=axs[1]
+    data = np.asarray(
+        [
+            selected_method(target[feature == category])
+            for category in feature_categories
+        ]
     )
-    axs[1].set_title(f"Distribution of {data[features].name}")
-    if labels is None:
-        axs[1].legend(loc="best")
-    else:
-        axs[1].legend(labels, loc="best")
-    return axs
+
+    final_bootstrap = np.empty([n_boots, len(feature_categories)])
+    for i in range(n_boots):
+        boots_index = np.random.choice(feature, size=len(feature), replace=True)
+        boots_temp = [
+            selected_method(target[boots_index][feature[boots_index] == category])
+            for category in feature_categories
+        ]
+        final_bootstrap[i, :] = boots_temp
+
+    percentile = np.percentile(final_bootstrap, (2.5, 97.5), axis=0)
+
+    if ax is None:
+        fig, ax = plt.subplots()
+
+    ax = _plot_barh(
+        feature_categories,
+        data,
+        add_label=True,
+        title=title,
+        x_label=f"Percentage of target compared to {method}",
+        y_label="Feature categories",
+        ax=ax,
+        yerr=percentile,
+    )
+
+    ax.axvline(y=selected_method(feature), linestyle="--", color="red")
+
+    return ax

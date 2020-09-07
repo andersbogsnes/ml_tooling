732560aabe2aed4915adf7ad4ba5b622041416d7
Alexander <alexander_guldbrandsen@hotmail.com>
2020-09-07 10:00:19 +0200

    Added property to get feature names from dataset object
    With this enhancement the user can get feature names in a more intuitive way

--- src/ml_tooling/data/base_data.py
+++ src/ml_tooling/data/base_data.py
@@ -113,6 +113,10 @@ class Dataset(metaclass=abc.ABCMeta):
 
         return x, y
 
+    @property
+    def features(self):
+        return [feature_name for feature_name in self.x.columns]
+
     def _load_prediction_data(self, *args, **kwargs) -> pd.DataFrame:
         pred_data = self.load_prediction_data(*args, **kwargs)
         if pred_data.empty:

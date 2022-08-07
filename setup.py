044e9e169a2c4e50bf2f4de2bbca573a89448e20
Anders Bogsnes <andersbogsnes@gmail.com>
2022-08-07 22:25:04 +0200

    Drop 3.7

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ setup(
     classifiers=CLASSIFIERS,
     keywords=KEYWORDS,
     zip_safe=False,
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     packages=PACKAGES,
     install_requires=INSTALL_REQUIRES,
     package_dir={"": "src"},
